b9d1a2dfa685426f2ed5dd91cbcbfca4112e8885
a6Brgeuka <a6Brgeuka@gmail.com>
2017-03-01 14:59:13 +0300

    change locationType to auto

--- config/environment.js
+++ config/environment.js
@@ -6,7 +6,7 @@ module.exports = function (environment) {
     podModulePrefix: 'ember-search-likes/pods',
     environment: environment,
     rootURL: '/',
-    locationType: 'hash',
+    locationType: 'auto',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
