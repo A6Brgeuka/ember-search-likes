1788567842c6f30c0e818f9907abc16dc0abe06e
a6Brgeuka <a6Brgeuka@gmail.com>
2017-03-14 22:58:34 +0300

    add friend index page

--- app/pods/authenticated/friends/controller.js
+++ app/pods/authenticated/friends/controller.js
@@ -12,80 +12,80 @@ export default Controller.extend({
 
 
   // ----- Overridden properties -----
-  queryParams: [
-    {'searchInput': 'search'}
-  ],
-
-
-  // ----- Static properties -----
-  off: 'off',
-  on:  'on',
-
-  searchInput: '',
-  isMale:      'off',
-  isFemale:    'off',
-
-
-  // ----- Computed properties -----
-  users:       reads('model.friends'),
-  maleUsers:   filterBy('users', 'sex', 2),
-  femaleUsers: filterBy('users', 'sex', 1),
-
-
-  isAll: and(equal('isMale', 'off'), equal('isFemale', 'off')),
-
-  maleOrFemaleOrAllUsers: conditional(
-    'isAll',
-    'users',
-    conditional(
-      equal('isMale', 'on'),
-      'maleUsers',
-      conditional(
-        equal('isFemale', 'on'),
-        'femaleUsers',
-        'users'
-      )
-    ),
-  ),
-
-  usersFiltered: searchArrayByProp('maleOrFemaleOrAllUsers', 'name', 'searchInput'),
-
-
-
-  // ----- Overridden Methods -----
-
-
-
-  // ----- Custom Methods -----
-
-
-
-  // ----- Events and observers -----
-
-
-
-  // ----- Tasks -----
-
-
-
-  // ----- Actions -----
-  actions: {
-    toggleMale (obj) {
-      if (obj.newValue === 'on') {
-        this.set('isMale', 'on')
-        this.set('isFemale', 'off')
-      } else {
-        this.set('isMale', 'off')
-      }
-    },
-
-    toggleFemale (obj) {
-      if (obj.newValue === 'on') {
-        this.set('isFemale', 'on')
-        this.set('isMale', 'off')
-      } else {
-        this.set('isFemale', 'off')
-      }
-    }
-  }
+  // queryParams: [
+  //   {'searchInput': 'search'}
+  // ],
+  //
+  //
+  // // ----- Static properties -----
+  // off: 'off',
+  // on:  'on',
+  //
+  // searchInput: '',
+  // isMale:      'off',
+  // isFemale:    'off',
+  //
+  //
+  // // ----- Computed properties -----
+  // users:       reads('model.friends'),
+  // maleUsers:   filterBy('users', 'sex', 2),
+  // femaleUsers: filterBy('users', 'sex', 1),
+  //
+  //
+  // isAll: and(equal('isMale', 'off'), equal('isFemale', 'off')),
+  //
+  // maleOrFemaleOrAllUsers: conditional(
+  //   'isAll',
+  //   'users',
+  //   conditional(
+  //     equal('isMale', 'on'),
+  //     'maleUsers',
+  //     conditional(
+  //       equal('isFemale', 'on'),
+  //       'femaleUsers',
+  //       'users'
+  //     )
+  //   ),
+  // ),
+  //
+  // usersFiltered: searchArrayByProp('maleOrFemaleOrAllUsers', 'name', 'searchInput'),
+  //
+  //
+  //
+  // // ----- Overridden Methods -----
+  //
+  //
+  //
+  // // ----- Custom Methods -----
+  //
+  //
+  //
+  // // ----- Events and observers -----
+  //
+  //
+  //
+  // // ----- Tasks -----
+  //
+  //
+  //
+  // // ----- Actions -----
+  // actions: {
+  //   toggleMale (obj) {
+  //     if (obj.newValue === 'on') {
+  //       this.set('isMale', 'on')
+  //       this.set('isFemale', 'off')
+  //     } else {
+  //       this.set('isMale', 'off')
+  //     }
+  //   },
+  //
+  //   toggleFemale (obj) {
+  //     if (obj.newValue === 'on') {
+  //       this.set('isFemale', 'on')
+  //       this.set('isMale', 'off')
+  //     } else {
+  //       this.set('isFemale', 'off')
+  //     }
+  //   }
+  // }
 })
